634196fb5cdc7b2d0df508c96103f6e52569acff
ctt10 <adamantspinner@protonmail.com>
2021-04-08 17:56:10 +0800

    updated

--- IMT/migrations/2_IMT.js
+++ IMT/migrations/2_IMT.js
@@ -1,7 +1,7 @@
 const IMT = artifacts.require("ERC20");
 
 module.exports = function (deployer) {
-	const _name = "Integrated Music Token"
+	const _name = "Integrated Music Token";
 	const _symbol = "IMT";
   deployer.deploy(IMT, _name, _symbol);
 };
--- TokenLock/migrations/3_TokenLock.js
+++ TokenLock/migrations/3_TokenLock.js
@@ -1,6 +1,6 @@
 const TokenLock = artifacts.require("TokenLock");
 
 module.exports = function (deployer) {
-  _token = '0x99b19bCC060e9e955B667BA11fF9c8f830095dA4'; //insert the ID of contract that was deployed from 2_IMT.js
+  _token = '0x20a830C126cB9846a8a239581a5F8a61efa872cB'; //insert the ID of contract that was deployed from 2_IMT.js
   deployer.deploy(TokenLock, _token);
 };
