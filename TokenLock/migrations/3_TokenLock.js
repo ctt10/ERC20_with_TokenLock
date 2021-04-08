8c68ff5a18335b0f05d41e47762709a75fb49498
ctt10 <adamantspinner@protonmail.com>
2021-04-08 12:16:55 +0800

    initial commit

---
+++ TokenLock/migrations/3_TokenLock.js
@@ -0,0 +1,6 @@
+const TokenLock = artifacts.require("TokenLock");
+
+module.exports = function (deployer) {
+  _token =  ; //insert the ID of contract that was deployed from 2_IMT.js
+  deployer.deploy(TokenLock _token);
+};
